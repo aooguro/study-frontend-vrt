02c535bea5e4d7f5c0ac60bb72b0e74d773453db
Oguro Aika <oguro.aika.cx@mynavi.jp>
2025-09-01 11:14:24 +0900

    add: storycap & reg-suit

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,10 @@
-import type { Preview } from '@storybook/nextjs-vite'
 import "../app/globals.css";
+import type { Preview } from '@storybook/nextjs-vite'
+import { withScreenshot } from "storycap";
+
+export const decorators = [
+  withScreenshot,
+];
 
 const preview: Preview = {
   parameters: {
@@ -9,14 +14,16 @@ const preview: Preview = {
        date: /Date$/i,
       },
     },
-
     a11y: {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
       test: 'todo'
-    }
-  },
+    },
+    screenshot: {
+      // Put global screenshot parameters(e.g. viewport)
+    },
+  }
 };
 
 export default preview;
